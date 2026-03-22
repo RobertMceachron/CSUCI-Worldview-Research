e129776084eb0cccf510a5cdb1b090fd85374326
Robert McEachron <contact@robertmceachron.com>
2026-03-22 11:21:36 -0700

    Refactor data processing and analysis in Analysis.R
    Updated data processing and analysis steps in Analysis.R, including library additions, data cleaning, and descriptive statistics calculations.

--- Analysis.R
+++ Analysis.R
@@ -1,87 +1,249 @@
 library(readxl) # For Reading XLSX
 library(lavaan) # For FA
 library(semPlot) # For FA Plots
-library(tidyverse) # For Plots
+library(tidyverse) # For Everything
 library(haven) # For SPSS Output
+library(stats) # For PCA
+library(car) # For Assumption Checks
+library(ltm) # For Reliability 
+library(psych) # For Descriptive
+library(apaTables) # For APA Tables 
 
-dataraw1 <- read_xlsx("data/WV_V1.xlsx") # Inputs 1st Survey
-dataraw2 <- read_xlsx("data/WV_V2.xlsx") # Inputs 2nd Survey
-
+  # Reads Scored Qualtrics File
+    dataraw1 <- read_xlsx("data/WV_V1.xlsx") # Inputs 1st Survey
+    dataraw2 <- read_xlsx("data/WV_V2.xlsx") # Inputs 2nd Survey
+  
+  # Reads All Variable Qualtrics Files
+    allcolumns1 <- read_xlsx("data/V1_All.xlsx") # Inputs 1st Survey
+    allcolumns2 <- read_xlsx("data/V2_All.xlsx") # Inputs 2nd Survey
 
 ########################
 #    Data Cleaning     #
 ########################
 
-# Removes Extra Rows & Combines Data Sets
-  data1trans <- dataraw1[,-c(1,13)]
-  data2trans <- dataraw2
-  data <- rbind(data1trans, data2trans)
+  # Removes Extra Rows 
+    data1trans <- dataraw1
+    data2trans <- dataraw2
+  
+  # Removes Pesky Qualtrics Labels >:(
+    data1trans <- data1trans[-1,]
+    data2trans <- data2trans[-c(1,83),]
+    
+  # Combines Data Sets  
+    data <- rbind(data1trans, data2trans)
+    
+  # Remove Outliers
+    data <- data[-c(99, 100),]
+    
+  # Give New Column Names
+    colnames(data) <- c("ID", "Age", "Gender", "Race", "MathAnxiety", "MathSelfConcept", "Religiosity",
+                        "SocioeeconomicStatus", "MathAvoidance", "SelfInvestmentMutability",
+                        "SelfInvestmentLOR", "EducationSES", "ParentClass", "SESrank", "Affluency",
+                        "Prestige", "WAIMutability", "WAIRelationToAuthority", "WAIMetaphysics", 
+                        "WAILocusOfResponsibility", "WAIAgency", "WAIRelationToGroup")
+    
+  # Converts to Numeric
+    data <- data %>% 
+            mutate_at(c("Age", "MathAnxiety", "MathAvoidance", "MathSelfConcept",
+                        "ParentClass", "Affluency", "Religiosity", "EducationSES", "SocioeeconomicStatus",
+                        "SESrank", "Prestige", "SelfInvestmentMutability", "SelfInvestmentLOR", 
+                        "WAIMutability", "WAIRelationToAuthority", "WAIMetaphysics", "WAILocusOfResponsibility",
+                        "WAIAgency", "WAIRelationToGroup"), as.numeric)
 
-# Removes Pesky Qualtrics Labels >:(
-  data <- data[-1,]
+  # Only variables we need
+    cleandata <- data |>
+      dplyr::select(!c(ParentClass, Affluency, Religiosity, EducationSES, SocioeeconomicStatus,
+                  SESrank, Prestige))
+    
+  # Outputs Combined Files for rest of Group
+    write_sav(data, "data/Combined_Data.sav")
+    write_csv(data, "data/Combined_Data.csv")
+  
+    
+###########################
+#     Normality Check     #
+###########################
+    
+  # WAI
+    hist(data |> 
+           dplyr::select(contains("WAIMUTABILITY")))
+    hist(data |> 
+           dplyr::select(contains("WAIRELATIONTOAUTHORITY")))
+    hist(data |> 
+           dplyr::select(contains("WAIMetaphysics")))
+    hist(data |> 
+           dplyr::select(contains("WAILocusOfResponsibility")))
+    hist(data |> 
+           dplyr::select(contains("WAIRelationtoGroup")))
+    
+  # Self Investment
+    hist(data |> 
+            dplyr::select(contains("SelfInvestmentMutability")))
+    hist(data |> 
+            dplyr::select(contains("SelfInvestmentLOR")))
+    
+  # Math
+    hist(data |> 
+           dplyr::select(contains("MathAnxiety")))
+    hist(data |> 
+           dplyr::select(contains("MathSelfConcept")))
+    hist(data |> 
+           dplyr::select(contains("MathAvoidance")))
 
-# Remove Outliers
-  
-# Convert to Numeric for analysis 
-  #data1$SC1 <- as.numeric(data1$SC19)  
-  
-  
-# Outputs SPSS File for rest of Group
-  write_sav(data, "data/Combined_Data.sav")
-  
-########################
-#     Demo Analysis    #
-########################
-
-  # Grabs Mean Age
-  data$Q2_Age <- as.numeric(data$Q2_Age)
-  mean(data$Q2_Age)
-  
-  # Calculate proportion of Male/Female
-  
-  
-  # Run analysis on scores for the scales
-  
-  
-  
-  # Factor analysis on the 6 scored scales 
-  # two investment 
-
-
-
-
+  # Other Demographics
+    hist(data |> 
+           dplyr::select(contains("ParentClass")))
+    hist(data |> 
+           dplyr::select(contains("Affluency")))
+    hist(data |> 
+           dplyr::select(contains("Religiosity")))
+    hist(data |> 
+           dplyr::select(contains("EducationSES")))   
+    hist(data |> 
+           dplyr::select(contains("SocioeeconomicStatus")))
+    hist(data |> 
+           dplyr::select(contains("SESrank")))    
+    hist(data |> 
+           dplyr::select(contains("Prestige"))) 
+    
 ###############################
-#   Confirm Factor Analysis   #
+#     Reliability Checking    #
 ###############################
+    
+  # Removes Labels
+    allcolumns1 <- allcolumns1[-1,]
+    allcolumns2 <- allcolumns2[-1,]
+    
+  # Self Investment
+    SIScaleData1 <- allcolumns1 |> 
+      dplyr::select(contains("selfin"))
+    SIScaleData2 <- allcolumns2 |> 
+      dplyr::select(contains("selfin"))
+    SIScale <- rbind(SIScaleData1, SIScaleData2)
+    SISScale <- na.omit(SIScale)
+    # SE Mutability
+      SIMScale <- SIScale |>
+        dplyr::select(ends_with("M"))
+      cronbach.alpha(na.omit(SIMScale))
+    # SE Locus of Responsibility
+      SILORScale <- SIScale |>
+        dplyr::select(ends_with("L"))
+      cronbach.alpha(na.omit(SILORScale))
+    
+  # Agency
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("AGV_14","AGV_101","AGV_55","AGV_116","AGD_43","AGD_44","AGD_136","AGD_120")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q5","Q22","Q30","Q51","Q38","Q34","Q13","Q9")))
+    ScaleAgency <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleAgency))
 
-  # Interested in Principal Component Analysis?
-  # Run Confirmatory Factor Analysis on ? Whatever 10B is
-      # Add a scree plot
-      # Agency
-      # Mutability
-      # R2A
-      # R2g
-      # LOR
-      # Metaphysics
-      # Self Investment mutability
-      # Self Investment LOR
-  # Run another one with the 8 but with also the investment scales
+  # Mutability
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("MP_103","MP_100","MP_109","MP_110")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q11","Q32","Q44","Q19")))
+    ScaleMutabililty <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleMutabililty))   
+      
+  # Authority
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("AUL_135","AUL_145","AUL_115","AULAT_137","AULAT_87","AULAT_61")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q7","Q40","Q36","Q23","Q47","Q15")))
+    ScaleAuthority <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleAuthority))
 
+  # Relation to Group
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("GRPC_81","GRPC_89","GRPC_2","GRPC_50","GRPC_121","GRPC_122","GRPI_108","GRPI_70","GRPI_62","GRPI_15","GRPI_54","GRPI_104")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q2","Q6","Q18","Q26","Q39","Q43","Q14","Q35","Q52","Q31","Q10","Q54")))
+    ScaleRelationToGroup <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleRelationToGroup))   
+   
+  # Locus of Responsibility
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("RESPE_150","RESPE_126","RESPE_4","RESPE_132","RESPE_133","RESPE_91","RESPE_49","RESPI_140","RESPI_66","RESPI_72","RESPI_149","RESPI_97","RESPI_69","RESPI_56")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q1","Q8","Q21","Q25","Q46","Q42","Q27","Q48","Q17","Q29","Q33","Q50","Q4","Q53")))
+    ScaleLOR <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleLOR))
 
+  # Metaphysics
+    Q1 <- allcolumns1 |> 
+      dplyr::select(ends_with(c("METAS_139","METAS_58","METAS_8","METAS_143","METAS_138","METAM_148","METAM_96","METAM_142","METAM_41","METAM_92")))
+    Q2 <- allcolumns2 |>
+      dplyr::select(ends_with(c("Q3","Q20","Q24","Q41","Q49","Q16","Q37","Q45","Q28","Q12")))
+    ScaleMetaphysics <- rbind(Q1, setNames(Q2, names(Q1)))
+    cronbach.alpha(na.omit(ScaleMetaphysics))
+    
+    
+#################################
+#     Descriptive Statistics    #
+#################################
+  
+  # Computes Mean, SD, SS for Variables
+    print(describe(data, na.rm = TRUE, omit=TRUE))
+    
+  # Checks # of M/F
+    print(table(data$Gender))
+  
+  # Checks # of Race Category
+    Race <- data$Race  
+    sum(str_count(Race, pattern = "White"))
+    sum(str_count(Race, pattern = "Black"))
+    sum(str_count(Race, pattern = "Asian"))
+    sum(str_count(Race, pattern = "Middle Eastern"))
+    sum(str_count(Race, pattern = "Chicano/Chicana"))
+    sum(str_count(Race, pattern = "I Identify as"))
+    sum(str_count(Race, pattern = "Prefer Not to Say")) 
+  
+  # Creates Correlation Table
+    apa.cor.table(
+      data, filename="ex.CorTable1.doc")
+    
 
+####################################
+#   Confirmatory Factor Analysis   #
+####################################
 
+    model <- 'Vision of Life =~ WAIMetaphysics + WAIAgency + WAIRelationToGroup
+              Power in Life =~ WAILocusOfResponsibility
+              Pathway through Life =~ WAIMutability + WAIRelationToAuthority
+              '
+    cfa_result <- cfa(model, data = data)
+    summary(cfa_result, standardized=TRUE)
+    semPaths(cfa_result, whatLabels="est",
+             sizeMan = 5,
+             node.width = 1,
+             edge.label.cex = .75,
+             style = "ram",
+             mar = c(5, 5, 5, 5))
+    
+####################################
+#   Exploratory Factor Analysis    #
+####################################
+    
+  # Selects Columns we want to analyze
+    EFAdata <- data |>
+                dplyr::select(WAIMutability, WAIRelationToAuthority, WAIMetaphysics, 
+                              WAILocusOfResponsibility, WAIAgency, WAIRelationToGroup)
+  # Creates Eigenvalues for factors  
+    eigenvalues <- eigen(cor(EFAdata))$values
+    print(eigenvalues)
+  
+  # Creates Scree Plot for Eigenvalues  
+    scree_plot <- data.frame(
+      eigenvalues = eigen(cor(EFAdata))$values,
+      component = 1:length(eigen(cor(EFAdata))$values)
+    )
 
-
-
-########################
-#   FACTOR ANALYSIS    #
-########################
-
-
-# Creates the model
-  model <- 'Worldview =~ SC1 + SC2 + SC3 + SC4 + SC5 + SC6 + SC12 + SC13'
-  cfa_result <- cfa(model, data = data)
-  summary(cfa_result)
-  semPaths(cfa_result)
-  semPaths(cfa_result, "std")
-
+    plot(scree_plot$component, scree_plot$eigenvalues, type = "b",
+         xlab = "Component Number", ylab = "Eigenvalue",
+         main = "Scree Plot")
+    abline(h = 1, col = "red", lty = 2)
+    
+  # Runs EFA
+    efa_result <- fa(r = EFAdata, nfactors = 3, rotate = "varimax")
+    print(efa_result)
